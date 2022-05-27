06e65e89ebfb5eb1343c2c5f73b28cdfcede11b4
Alexkings47 <michealexcel54@gmail.com>
2022-05-27 16:19:31 +0100

    cleared redundancy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home"
 import Houses from "./pages/properties"
 import Agents from "./pages/agents"
-import NewAdditions from "./components/NewAdditions";
 import NewProperty from "./pages/newproperty"
 
 
